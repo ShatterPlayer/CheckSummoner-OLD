527f63baccdf9f39297de3ba9f7836aeac1fdd94
ShatterPlayer <shatterplayer@gmail.com>
2018-08-16 12:15:58 +0200

    Clear cache

--- index.php
+++ index.php
@@ -15,11 +15,10 @@
 	</script>
 
 	<!--  Clear Cache -->
-	<meta http-equiv="cache-control" content="max-age=0" />
-	<meta http-equiv="cache-control" content="no-cache" />
-	<meta http-equiv="expires" content="0" />
-	<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
-	<meta http-equiv="pragma" content="no-cache" />
+	<meta http-equiv="Expires" content="0" />
+	<meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate" />
+	<meta http-equiv="Cache-Control" content="post-check=0, pre-check=0" />
+	<meta http-equiv="Pragma" content="no-cache" />
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 	
